16ea6b5f4199f2dfe89cc0e3d87c271ee6aadcd5
Jamie Hall <jamie.m.hall@outlook.com>
2021-10-08 12:19:23 +0100

    Add an HTML response

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+import uvicorn
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-  
+async def root(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request, 'output': 'Hello World'})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
