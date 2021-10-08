63b94ca6fc802bd81b4b9d99012187de06941580
Jamie Hall <jamie.m.hall@outlook.com>
2021-10-08 14:57:31 +0100

    Introducing routes in templates

--- routers/crypto.py
+++ routers/crypto.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(prefix='/crypto')
 
 templates = Jinja2Templates(directory="templates/")
 
 
-@router.get('/crypto')
+@router.get('/')
 def list_coins(request: Request):
     return templates.TemplateResponse('list_crypto_coins.html', context={'request': request, 'output': ['BTC', 'ETH', 'XRP']})
